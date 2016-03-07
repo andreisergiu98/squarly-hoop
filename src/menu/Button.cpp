368ecbd848cc8abf241e95098c0062729f6580ab
andreisergiu98 <andrei.sergiu98@gmail.com>
2016-03-07 22:41:06 +0200

    Add buttons and some textures

---
+++ src/menu/Button.cpp
@@ -0,0 +1,59 @@
+////////////////////////////////////////////////////////////
+//
+//  squarly-hoop
+//	Copyright (C) 2016  Pampu Andrei (aka. Woky) (andrei.sergiu98@gmail.com)
+//
+//	This program is free software: you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published by
+//	the Free Software Foundation, either version 3 of the License, or
+//	any later version.
+//
+//	This program is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
+//
+//	You should have received a copy of the GNU General Public License
+//	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+////////////////////////////////////////////////////////////
+
+#include "Button.h"
+
+Button::Button(sf::Vector2f position, sf::Vector2f size, sf::Texture &texNormal, sf::Texture &texMouseOver) {
+    form.setPosition(position);
+    form.setSize(size);
+    form.setTexture(&texNormal);
+    mouse.width = 5;
+    mouse.height = 5;
+
+    this->texNormal = &texNormal;
+    this->texMouseOver = &texMouseOver;
+    status = NORMAL;
+}
+
+void Button::updateMouse(sf::Vector2i mousePosition) {
+    mouse.top = mousePosition.y;
+    mouse.left = mousePosition.x;
+}
+
+void Button::update() {
+    if (status == NORMAL and form.getGlobalBounds().intersects(mouse)) {
+        form.setTexture(texMouseOver);
+       // form.setFillColor(sf::Color::Blue);
+        status = MOUSEOVER;
+    }
+    if (status == MOUSEOVER and !form.getGlobalBounds().intersects(mouse)) {
+        form.setTexture(texNormal);
+       // form.setFillColor(sf::Color::Red);
+        status = NORMAL;
+    }
+}
+
+bool Button::isPressed() {
+    return status == MOUSEOVER and sf::Mouse::isButtonPressed(sf::Mouse::Button::Left);
+}
+
+void Button::draw(sf::RenderTarget &target, sf::RenderStates states) const {
+    target.draw(form);
+}
---
+++ src/menu/Button.h
@@ -0,0 +1,49 @@
+////////////////////////////////////////////////////////////
+//
+//  squarly-hoop
+//	Copyright (C) 2016  Pampu Andrei (aka. Woky) (andrei.sergiu98@gmail.com)
+//
+//	This program is free software: you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published by
+//	the Free Software Foundation, either version 3 of the License, or
+//	any later version.
+//
+//	This program is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
+//
+//	You should have received a copy of the GNU General Public License
+//	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+////////////////////////////////////////////////////////////
+
+#ifndef SFMLGAME_BUTTON_H
+#define SFMLGAME_BUTTON_H
+
+#include <SFML/Graphics.hpp>
+
+class Button : public sf::Drawable {
+public:
+    Button(){}
+    Button(sf::Vector2f position, sf::Vector2f size, sf::Texture &texNormal, sf::Texture &texMouseOver);
+
+    void update();
+    void updateMouse(sf::Vector2i);
+    bool isPressed();
+
+private:
+    sf::RectangleShape form;
+    sf::FloatRect mouse;
+    sf::Texture *texNormal;
+    sf::Texture *texMouseOver;
+
+    enum Mouse{
+        MOUSEOVER, NORMAL, ISPRESSED};
+    Mouse status;
+
+    void draw(sf::RenderTarget &target, sf::RenderStates states) const;
+};
+
+
+#endif //SFMLGAME_BUTTON_H
