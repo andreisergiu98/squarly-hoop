ead4906909bd9b490721650ba4494f8e6084586e
andreisergiu98 <andrei.sergiu98@gmail.com>
2016-03-20 19:35:56 +0200

    Add mit license for beat detection

--- src/music/TimeStamp.h
+++ src/music/TimeStamp.h
@@ -1,35 +1,61 @@
+////////////////////////////////////////////////////////////
+//
+//      Copyright (c) 2015 Padraig O Connor
+//
+//      Permission is hereby granted, free of charge, to any person obtaining a copy
+//      of this software and associated documentation files (the "Software"), to deal
+//      in the Software without restriction, including without limitation the rights
+//      to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//      copies of the Software, and to permit persons to whom the Software is
+//      furnished to do so, subject to the following conditions:
+//      
+////////////////////////////////////////////////////////////  
+
 #ifndef TIMESTAMP
 #define TIMESTAMP
 
-class TimeStamp
-{
+class TimeStamp {
 public:
-	TimeStamp();
-	TimeStamp(int,int,int);
-	TimeStamp(int, int, int, float);
-	TimeStamp(int, int, int, char*);
-	~TimeStamp();
+    TimeStamp();
 
-	void setTime(int, int, int);
-	void setSeconds(int);
-	void setMinutes(int);
-	void setMilliseconds(int);
-	void setFrequency(float);
+    TimeStamp(int, int, int);
 
-	int getSeconds();
-	int getMinutes();
-	int getMilliseconds();
-	float getFrequency();
-	char* getMetaData();
+    TimeStamp(int, int, int, float);
 
-	void setTimeWithMetaData(int, int, int, char*);
-	void setTimeWithFrequency(int, int, int, float);
+    TimeStamp(int, int, int, char *);
+
+    ~TimeStamp();
+
+    void setTime(int, int, int);
+
+    void setSeconds(int);
+
+    void setMinutes(int);
+
+    void setMilliseconds(int);
+
+    void setFrequency(float);
+
+    int getSeconds();
+
+    int getMinutes();
+
+    int getMilliseconds();
+
+    float getFrequency();
+
+    char *getMetaData();
+
+    void setTimeWithMetaData(int, int, int, char *);
+
+    void setTimeWithFrequency(int, int, int, float);
+
 private:
-	int minutes;
-	int seconds;
-	int milliseconds;
-	char* metaData;
-	float beatFrequency;
+    int minutes;
+    int seconds;
+    int milliseconds;
+    char *metaData;
+    float beatFrequency;
 };
 
 #endif
